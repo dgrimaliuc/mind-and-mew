72bd970a7d2ce062aeaf3373d5c3c183dd05fa81
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2025-01-20 23:11:07 +0200

    Added babel config

--- eslint.config.js
+++ eslint.config.js
@@ -1,42 +1,43 @@
-import importPlugin from 'eslint-plugin-import';
-import jestPlugin from 'eslint-plugin-jest';
 import babelParser from '@babel/eslint-parser';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginJest from 'eslint-plugin-jest';
 
 export default [
   {
-    ignores: ['node_modules', 'dist'], // Replace your .eslintignore content here
+    ignores: ['node_modules', 'dist'], // Adjust as needed
   },
   {
-    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'], // Adjust for your project
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'module',
       parser: babelParser,
       parserOptions: {
         requireConfigFile: false,
+        ecmaFeatures: {
+          jsx: true, // Enable JSX syntax
+        },
       },
       globals: {
-        // Define environment-specific globals
-        process: 'readonly', // Node.js
-        module: 'readonly', // Node.js
-        require: 'readonly', // Node.js
-        __dirname: 'readonly', // Node.js
-        jest: 'readonly', // Jest testing
-        describe: 'readonly', // Jest
-        it: 'readonly', // Jest
-        expect: 'readonly', // Jest
+        process: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        jest: 'readonly',
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
       },
     },
     plugins: {
-      import: importPlugin,
-      jest: jestPlugin,
-    },
+      import: eslintPluginImport,
+      jest: eslintPluginJest,
+    }, // Correct plugin definition
     rules: {
       'no-console': 'off',
       'no-unused-vars': 'warn',
       'no-inner-declarations': 'off',
       'import/no-unresolved': 'error',
-      'import/named': 'error',
       'jest/no-disabled-tests': 'warn',
       'jest/no-focused-tests': 'error',
       'jest/no-identical-title': 'error',
